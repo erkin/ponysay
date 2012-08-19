e3176be013f6aa5b4d9a151f824e8a2067a4cf4b
Elis Axelsson <elis.axelsson@gmail.com>
2012-08-19 10:54:33 +0200

    Updated fish-completions again, removed some quotes, changed to long-options to make it more clear, added option for the message

--- completion/fish-completion.fish
+++ completion/fish-completion.fish
@@ -3,18 +3,18 @@
 #
 # Author: Elis Axelsson <etu AT elis DOT nu>
 
-set -g ponies  ('/usr/bin/ponysay.py' --onelist)
-set -g quoters ('/usr/bin/ponysay.py' --quoters)
+set -g ponies  (/usr/bin/ponysay.py --onelist)
+set -g quoters (/usr/bin/ponysay.py --quoters)
 
 
-complete -c ponysay -s h -l help                  --description "help of ponysay"
-complete -c ponysay -s v -l version               --description "version of ponysay"
-complete -c ponysay -s l -l list                  --description "list pony files"
-complete -c ponysay -s L -l altlist               --description "list pony files with alternatives"
-complete -c ponysay -s f -l pony    -a "$ponies"  --description "select a pony"
-complete -c ponysay -s q -l quote   -a "$quoters" --description "select a pony which will quote herself"
-complete -c ponysay -s W -l wrap    -a "Integer"  --description "specify the column when the message should be wrapped"
-
+complete --command ponysay --short-option h --long-option help                                      --description 'help of ponysay'
+complete --command ponysay --short-option v --long-option version                                   --description 'version of ponysay'
+complete --command ponysay --short-option l --long-option list                                      --description 'list pony files'
+complete --command ponysay --short-option L --long-option altlist                                   --description 'list pony files with alternatives'
+complete --command ponysay --short-option f --long-option pony    --arguments "$ponies"             --description 'pony'
+complete --command ponysay --short-option q --long-option quote   --arguments "$quoters" --no-files --description 'pony'
+complete --command ponysay --short-option W --long-option wrap    --arguments 'Integer'             --description 'specify the column when the message should be wrapped'
+complete --command ponysay                                        --arguments 'MESSAGE'
 
 set -e ponies
 set -e quoters
